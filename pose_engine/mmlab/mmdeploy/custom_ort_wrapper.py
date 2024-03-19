7de11ff90445b7f5359e28e78efa0fbf3975e30b
btalberg <ben.talberg@gmail.com>
2024-03-19 14:58:31 -0500

    Update gitignore to include accidentally ignored subfoldrs

---
+++ pose_engine/mmlab/mmdeploy/custom_ort_wrapper.py
@@ -0,0 +1,21 @@
+from typing import Optional, Sequence
+
+from mmengine.runner import load_checkpoint
+from mmdeploy.utils import Backend
+from mmdeploy.backend.base import BACKEND_MANAGERS
+from mmdeploy.backend.onnxruntime import ORTWrapper
+
+
+@BACKEND_MANAGERS.register(Backend.ONNXRUNTIME.value)
+class CustomORTWrapper(ORTWrapper):
+    def __init__(self,
+                 onnx_file: str,
+                 device: str,
+                 output_names: Optional[Sequence[str]] = None):
+        
+        checkpoint = load_checkpoint(onnx_file, map_location="cpu")
+        super(CustomORTWrapper, self).__init__(
+            onnx_file=onnx_file,
+            device=device,
+            output_names=output_names
+        )
